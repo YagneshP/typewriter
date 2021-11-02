73a9980eb98ca16e572669b4d66821595132c638
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-02 14:44:22 -0400

    printing new line after completing loop

--- typewriter.js
+++ typewriter.js
@@ -1,14 +1,7 @@
 const sentence = "hello there from lighthouse labs";
 let delay = 0;
-for (const i in sentence) {
-  if(Number(i) === (sentence.length - 1)){
-    setTimeout(() => {
-      process.stdout.write(`${sentence[i]}\n`);
-    }, delay);
-  }else{
-    setTimeout(() => {
-      process.stdout.write(sentence[i]);
-    }, delay);
-  }
+for (const val of sentence) {
+  setTimeout(() => process.stdout.write(val), delay);
   delay += 50;
 }
+setTimeout(() => process.stdout.write(`\n`), delay);
